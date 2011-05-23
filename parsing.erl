5132ad8930d0732e14e8fea29f8a0f5525bf46bd
Mirko Kiefer <mail@mirkokiefer.com>
2011-05-23 21:19:17 +0200

    nodes and ways being parsed

--- parsing.erl
+++ parsing.erl
@@ -5,32 +5,47 @@ run() ->
   {ok, Xml} = file:read_file("data/map.osm"),
   erlsom:parse_sax(Xml, [], fun event/2).
 
+parseAttributes(Attributes) ->
+  [{Attr, Value} || {_, Attr, _, _, Value} <- Attributes].
 
 filterAttributes(Attributes, FilterAttributes) ->
-  [[Value || {Attr, Value} <- Attributes, Attr == FilterAttr] ||
+  ParsedAttributes = parseAttributes(Attributes),
+  [lists:nth(1, [Value || {Attr, Value} <- ParsedAttributes, Attr == FilterAttr]) ||
       FilterAttr <- FilterAttributes].
 
 event(Event = {startElement, _, "node", _, Attributes}, _) ->
-  ParsedAttributes = [{Attr, Value} || {_, Attr, _, _, Value} <- Attributes],
-  [[ID], [Lat], [Lon]] = [[Value ||
-    {Attr, Value} <- ParsedAttributes, Attr == FilterAttr] ||
-      FilterAttr <- ["id", "lat", "lon"]],
+  ParsedAttributes = parseAttributes(Attributes),
+  [ID, Lat, Lon] = filterAttributes(Attributes, ["id", "lat", "lon"]),
   State = {{id, ID, lat, Lat, lon, Lon}, []},
   State;
 
 event(Event = {startElement, _, "tag", _, Attributes}, _State = {Node, Details}) ->
-  ParsedAttributes = [{Attr, Value} || {_, Attr, _, _, Value} <- Attributes],
-  [[K], [V]] = [[Value ||
-    {Attr, Value} <- ParsedAttributes, Attr == FilterAttr] ||
-      FilterAttr <- ["k", "v"]],
-  NewState = {Node,[{K, V} | Details]},
+  ParsedAttributes = parseAttributes(Attributes),
+  [K, V] = filterAttributes(Attributes, ["k", "v"]),
+  NewState = {Node,[{attr, K, V} | Details]},
   NewState;
 
 event(Event = {endElement, _, "node", _}, State) ->
   io:format("end node: ~p~n", [State]),
   [];
 
-%% Catch-all. Pass state on as-is    
+event(Event = {startElement, _, "way", _, Attributes}, _) ->
+  ParsedAttributes = parseAttributes(Attributes),
+  [ID] = filterAttributes(Attributes, ["id"]),
+  State = {{id, ID}, []},
+  State;
+
+event(Event = {startElement, _, "nd", _, Attributes}, _State = {Node, Details}) ->
+  ParsedAttributes = parseAttributes(Attributes),
+  [Ref] = filterAttributes(Attributes, ["ref"]),
+  NewState = {Node,[{ref, Ref} | Details]},
+  NewState;
+
+event(Event = {endElement, _, "way", _}, State) ->
+  io:format("end way: ~p~n", [State]),
+  [];
+
+%% Catch-all. Pass state on as-is
 event(Event, State) ->
-  io:format("~p~n", [Event]),
+  %% io:format("~p~n", [Event]),
   State.
