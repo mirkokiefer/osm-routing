4b66b22923133e5de9dcb8c7923ad2cac493639e
Mirko Kiefer <mail@mirkokiefer.com>
2011-05-31 18:26:56 +0200

    parsing and processing works, outputs databases as .tab files

--- parsing.erl
+++ parsing.erl
@@ -1,9 +1,14 @@
 -module(parsing).
 -export([run/0]).
 
+
 run() ->
   {ok, Xml} = file:read_file("data/map.osm"),
-  erlsom:parse_sax(Xml, [], fun event/2).
+  ets:new(osm_nodes, [named_table, set, public]),
+  ets:new(osm_ways, [named_table, set, public]),
+  erlsom:parse_sax(Xml, [], fun event/2),
+  ets:tab2file(osm_nodes, "output/osm_nodes.tab"),
+  ets:tab2file(osm_ways, "output/osm_ways.tab").
 
 parseAttributes(Attributes) ->
   [{Attr, Value} || {_, Attr, _, _, Value} <- Attributes].
@@ -13,39 +18,43 @@ filterAttributes(Attributes, FilterAttributes) ->
   [lists:nth(1, [Value || {Attr, Value} <- ParsedAttributes, Attr == FilterAttr]) ||
       FilterAttr <- FilterAttributes].
 
+
 event(Event = {startElement, _, "node", _, Attributes}, _) ->
-  ParsedAttributes = parseAttributes(Attributes),
   [ID, Lat, Lon] = filterAttributes(Attributes, ["id", "lat", "lon"]),
-  State = {{id, ID, lat, Lat, lon, Lon}, []},
+  State = {{id, list_to_atom(ID), lat, Lat, lon, Lon}, []},
   State;
 
-event(Event = {startElement, _, "tag", _, Attributes}, _State = {Node, Details}) ->
-  ParsedAttributes = parseAttributes(Attributes),
+event(Event = {startElement, _, "tag", _, Attributes}, State) ->
   [K, V] = filterAttributes(Attributes, ["k", "v"]),
-  NewState = {Node,[{attr, K, V} | Details]},
+  case State of
+    {Node, Tags} -> NewState = {Node,[{tag, K, V} | Tags]};
+    {Node, Tags, Refs} -> NewState = {Node,[{K, V} | Tags], Refs};
+    _Any -> NewState = State
+  end,
   NewState;
 
 event(Event = {endElement, _, "node", _}, State) ->
   io:format("end node: ~p~n", [State]),
+  {{id, ID, lat, Lat, lon, Lon}, Tags} = State,
+  ets:insert(osm_nodes, {ID, {lat, Lat}, {lon, Lon}, {tags, Tags}}),
   [];
 
 event(Event = {startElement, _, "way", _, Attributes}, _) ->
-  ParsedAttributes = parseAttributes(Attributes),
   [ID] = filterAttributes(Attributes, ["id"]),
-  State = {{id, ID}, []},
+  State = {{id, list_to_atom(ID)}, [], []},
   State;
 
-event(Event = {startElement, _, "nd", _, Attributes}, _State = {Node, Details}) ->
-  ParsedAttributes = parseAttributes(Attributes),
+event(Event = {startElement, _, "nd", _, Attributes}, _State = {Node, Tags, Refs}) ->
   [Ref] = filterAttributes(Attributes, ["ref"]),
-  NewState = {Node,[{ref, Ref} | Details]},
+  NewState = {Node, Tags, [list_to_atom(Ref) | Refs]},
   NewState;
 
 event(Event = {endElement, _, "way", _}, State) ->
   io:format("end way: ~p~n", [State]),
+  {{id, ID}, Tags, Refs} = State,
+  ets:insert(osm_ways, {ID, {tags, Tags}, {refs, Refs}}),
   [];
 
 %% Catch-all. Pass state on as-is
 event(Event, State) ->
-  %% io:format("~p~n", [Event]),
   State.
---
+++ processing.erl
@@ -0,0 +1,28 @@
+-module(processing).
+-export([run/0]).
+
+
+run() ->
+  ets:file2tab("output/osm_nodes.tab"),
+  ets:file2tab("output/osm_ways.tab"),
+  ets:new(osm_nodes_to_ways, [named_table, bag, public]),
+  processData(),
+  ets:tab2file(osm_nodes_to_ways, "output/osm_nodes_to_ways.tab").
+  
+processData() ->
+  FirstKey = ets:first(osm_ways),
+  [{ID, _, {refs, Refs}}] = ets:lookup(osm_ways, FirstKey),
+  writeNodesToWay(Refs, ID),
+  processData(ets:next(osm_ways, FirstKey)).
+
+processData('$end_of_table') ->
+  true;
+
+processData(NextKey) ->
+  [{ID, _, {refs, Refs}}] = ets:lookup(osm_ways, NextKey),
+  writeNodesToWay(Refs, ID),
+  io:format("read way: ~p~n", [ID]),
+  processData(ets:next(osm_ways, NextKey)).
+  
+writeNodesToWay(Refs, WayID) ->
+  lists:foreach(fun(Ref) -> ets:insert(osm_nodes_to_ways, {Ref, WayID}) end, Refs).
