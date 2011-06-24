59679b179a8ded85661b9693b101ba141d1e6974
Mirko Kiefer <mail@mirkokiefer.com>
2011-06-24 17:16:43 +0200

    start route descriptions

--- geodata.erl
+++ geodata.erl
@@ -1,9 +1,13 @@
 -module(geodata).
--export([route/2, edges/1, nodes_to_coords/1, distance/2, node2ways/1, lookup_way/1, lookup_node/1, coordinates/1]).
+-export([route/2, route_simple/2, edges/1, nodes_to_coords/1, distance/2, group_nodes/1, connecting_way/2, node2ways/1, lookup_way/1, lookup_node/1, coordinates/1]).
 
 
 route(SourceID, TargetID) ->
   astar:shortest_path(SourceID, TargetID).
+  
+route_simple(SourceID, TargetID) ->
+  [{path, Nodes}, _, _] = route(SourceID, TargetID),
+  Nodes.
   
 edges(NodeID) ->
   WayIds = node2ways(NodeID),
@@ -28,6 +32,30 @@ distance(NodeAID, NodeBID) ->
 
 nodes_to_coords(List) ->
   [geodata:coordinates(geodata:lookup_node(Node)) || Node <- List].
+  
+group_nodes(Nodes) ->
+  group_nodes(Nodes, [{way, undefined}, {nodes, []}], []).
+  
+group_nodes([First], [{way, Way}, {nodes, Nodes}], List) ->
+  NewGroup = [{way, Way}, {nodes, lists:reverse([First|Nodes])}],
+  [_Undefined | Rest] = lists:reverse([NewGroup|List]),
+  Rest;
+
+group_nodes([First|Rest], [{way, Way}, {nodes, Nodes}], List) ->
+  [Second|_] = Rest,
+  NewWay = connecting_way(First, Second),
+  case NewWay of
+    Way -> NewGroup = [{way, Way}, {nodes, [First|Nodes]}], NewList = List;
+    _ -> NewGroup = [{way, NewWay}, {nodes, [First]}],
+      NewList = [[{way, Way},{nodes, lists:reverse(Nodes)}] | List]
+  end,
+  group_nodes(Rest, NewGroup, NewList).
+  
+delta_angle(GroupedNodes) ->
+  %http://www.movable-type.co.uk/scripts/latlong.html.
+  
+delta_angle([First|Rest], List) ->
+
 
 % ets accessing functions:
 node2ways(NodeID) ->
@@ -70,6 +98,15 @@ neighbours(Element, Last, List) ->
     [First | Rest] -> Result = neighbours(Element, First, Rest)
   end,
   Result.
+  
+connecting_way(NodeA, NodeB) ->
+  WaysA = node2ways(NodeA),
+  WaysB = node2ways(NodeB),
+  case intersection(WaysA, WaysB) of
+    [] -> undefined;
+    [Way] -> Way;
+    [First | _Rest] -> First
+  end.
 
 deg2rad(Deg) -> Deg*math:pi()/180.
 
@@ -85,3 +122,4 @@ linkFromPath(Path) ->
   string:join(["http://maps.google.com/maps/api/staticmap?", "sensor=false",
     "&size=640x640", "&path=color:0x0000ff|weight:5|", Param], "").
   
+intersection(L1,L2) -> lists:filter(fun(X) -> lists:member(X,L1) end, L2).
