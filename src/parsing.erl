81bdc0101d5fedcb2e03dc357142974da92a390d
Mirko Kiefer <mail@mirkokiefer.com>
2011-09-19 21:25:20 +0200

    remove warnings in parsing

--- src/parsing.erl
+++ src/parsing.erl
@@ -3,12 +3,12 @@
 
 
 run() ->
-  {ok, Xml} = file:read_file("data/heidelberg_mannheim.osm"),
+  {ok, Xml} = file:read_file("../data/heidelberg_mannheim.osm"),
   ets:new(osm_nodes, [named_table, set, public]),
   ets:new(osm_ways, [named_table, set, public]),
   erlsom:parse_sax(Xml, [], fun event/2),
-  ets:tab2file(osm_nodes, "output/osm_nodes.tab"),
-  ets:tab2file(osm_ways, "output/osm_ways.tab").
+  ets:tab2file(osm_nodes, "../output/osm_nodes.tab"),
+  ets:tab2file(osm_ways, "../output/osm_ways.tab").
 
 parseAttributes(Attributes) ->
   [{Attr, Value} || {_, Attr, _, _, Value} <- Attributes].
@@ -19,12 +19,12 @@ filterAttributes(Attributes, FilterAttributes) ->
       FilterAttr <- FilterAttributes].
 
 
-event(Event = {startElement, _, "node", _, Attributes}, _) ->
+event({startElement, _, "node", _, Attributes}, _) ->
   [ID, Lat, Lon] = filterAttributes(Attributes, ["id", "lat", "lon"]),
   State = {{id, list_to_atom(ID), lat, Lat, lon, Lon}, []},
   State;
 
-event(Event = {startElement, _, "tag", _, Attributes}, State) ->
+event({startElement, _, "tag", _, Attributes}, State) ->
   [K, V] = filterAttributes(Attributes, ["k", "v"]),
   case State of
     {Node, Tags} -> NewState = {Node,[{tag, K, V} | Tags]};
@@ -33,23 +33,23 @@ event(Event = {startElement, _, "tag", _, Attributes}, State) ->
   end,
   NewState;
 
-event(Event = {endElement, _, "node", _}, State) ->
+event({endElement, _, "node", _}, State) ->
   %io:format("end node: ~p~n", [State]),
   {{id, ID, lat, Lat, lon, Lon}, Tags} = State,
   ets:insert(osm_nodes, {ID, {lat, Lat}, {lon, Lon}, {tags, Tags}}),
   [];
 
-event(Event = {startElement, _, "way", _, Attributes}, _) ->
+event({startElement, _, "way", _, Attributes}, _) ->
   [ID] = filterAttributes(Attributes, ["id"]),
   State = {{id, list_to_atom(ID)}, [], []},
   State;
 
-event(Event = {startElement, _, "nd", _, Attributes}, _State = {Node, Tags, Refs}) ->
+event({startElement, _, "nd", _, Attributes}, _State = {Node, Tags, Refs}) ->
   [Ref] = filterAttributes(Attributes, ["ref"]),
   NewState = {Node, Tags, [list_to_atom(Ref) | Refs]},
   NewState;
 
-event(Event = {endElement, _, "way", _}, State) ->
+event({endElement, _, "way", _}, State) ->
   %io:format("end way: ~p~n", [State]),
   {{id, ID}, Tags, Refs} = State,
   case lists:any(fun({K, _V})-> K == "name" end, Tags) of
@@ -59,5 +59,5 @@ event(Event = {endElement, _, "way", _}, State) ->
   [];
 
 %% Catch-all. Pass state on as-is
-event(Event, State) ->
+event(_Event, State) ->
   State.
