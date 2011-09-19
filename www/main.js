7c18dfcbbce9a5b5c4b75a33e8779105d24771b8
Mirko Kiefer <mail@mirkokiefer.com>
2011-09-19 20:06:11 +0200

    enable entering of nodes in UI

--- www/main.js
+++ www/main.js
@@ -1,28 +1,23 @@
-function getParameterByName(name) {
+var getParameterByName = function(name) {
   var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
   return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
 };
 
-$(function() {
-  var from = getParameterByName('from');
-  var to = getParameterByName('to');
-  $("#from").val(from);
-  $("#to").val(to);
-  
+var displayMap = function(from, to) {
   $.get('route?from=' + from + '&to=' + to, function(json) {
     var data = JSON.parse(json);
     var coords = data.route.map(function(each) {
       return new google.maps.LatLng(each.lat, each.lon);
     });
-
+    
     var myLatLng = new google.maps.LatLng(data.route[0].lat, data.route[0].lon);
     var myOptions = {
       zoom: 14,
       center: myLatLng,
       mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-     
+    }; 
     var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+    
     var path = new google.maps.Polyline({
       path: coords,
       map: map,
@@ -34,6 +29,7 @@ $(function() {
     $.get('route_description?from=' + from + '&to=' + to, function(routeDescriptionJson) {
       var routeDescription = JSON.parse(routeDescriptionJson).description;
       var index = 1;
+      displayRouteDescription(routeDescription);
       routeDescription.forEach(function(each) {
         var options = {
           position: new google.maps.LatLng(each.location.lat, each.location.lon),
@@ -45,4 +41,44 @@ $(function() {
       });
     });
   });
+};
+
+var displayEmptyMap = function() {
+  //focus on Heidelberg
+  var myLatLng = new google.maps.LatLng(49.3970139, 8.679436);
+  var myOptions = {
+    zoom: 14,
+    center: myLatLng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }; 
+  var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+};
+
+var displayRouteDescription = function(data) {
+  var text = data.map(function(each) {
+    return each.walk + '<br><br>' + each.direction + '<br>';
+  });
+  $('#directions').html(text.join(''));
+};
+
+var initForm = function() {
+  $('#submit').click(function() {
+    var from = $("#from").val();
+    var to = $("#to").val();
+    window.location = '?from=' + from + '&to=' + to;
+  });
+}
+
+$(function() {
+  var from = getParameterByName('from');
+  var to = getParameterByName('to');
+  $("#from").val(from);
+  $("#to").val(to);
+  displayMap(from, to);
+  if(from && to) {
+    displayMap(from, to);
+  } else {
+    displayEmptyMap();
+  }
+  initForm();
 });
