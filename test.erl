e5e55045fc28959e7420daa59db1e97256db022a
Mirko Kiefer <mail@mirkokiefer.com>
2011-09-19 15:24:28 +0200

    some tests

--- test.erl
+++ test.erl
@@ -29,6 +29,11 @@ test_short() ->
   test(Mirko, Haykuhi).
   
   % http://127.0.0.1:2904/route?source=26999575&target=1118741072
-  % http://127.0.0.1:2904/map?source=26999575&target=275283807
+  % http://127.0.0.1:2904/map?source=1118741072&target=60191569
+  % http://127.0.0.1:2904/map?source=1118741072&target=26999575
+  % http://127.0.0.1:2904/map?source=26999575&target=60191569  
+  % http://127.0.0.1:2904/map?source=26999575&target=275283807 
   % geodata:route_annotated('1118741072', '60191569')
   % geodata:route_description('1118741072', '60191569')
+  % astar:shortest_path_with_distances('1118741072', '60191569')
+  % requests:route_description('1118741072', '60191569')
