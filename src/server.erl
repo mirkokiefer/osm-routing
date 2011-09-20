4cda6a4fc89e70f7d191829a126b658b7d713fdf
Mirko Kiefer <mail@mirkokiefer.com>
2011-09-21 01:34:51 +0200

    re-implement API's to use mochiweb for JSON encoding

--- src/server.erl
+++ src/server.erl
@@ -22,10 +22,9 @@ respond("/route", [{"from", From}, {"to", To}], Req) ->
   try requests:route(list_to_atom(From), list_to_atom(To)) of
     [{path, Path}, {distance, Distance}, {stats, Stats}] ->
       Coords = nodes_to_coords(Path),
-      Res = {[{route, Coords}, {distance, Distance}, {stats, {Stats}}]},
-      {ok, Json} = json:encode(Res),
-      Body = io_lib:format("~s", [binary_to_list(Json)]),
-      Req:ok({"text/plain", Body})
+      Res = {struct, [{route, Coords}, {distance, Distance}, {stats, {struct, Stats}}]},
+      Json = mochijson2:encode(Res),
+      Req:ok({"text/plain;charset=utf-8", Json})
   catch
     _:X -> io:format("~p~n", [X])
   end;
@@ -33,16 +32,28 @@ respond("/route", [{"from", From}, {"to", To}], Req) ->
 respond("/route_description", [{"from", From}, {"to", To}], Req) ->
   try requests:route_description(list_to_atom(From), list_to_atom(To)) of
     Description ->
-      FormattedDescription = [{[{location, {node_to_coords(NodeID)}}, Distance, {walk, list_to_binary(Walk)},
-        {direction, list_to_binary(Direction)}]} ||
+      FormattedDescription = [{struct, [{location, node_to_coords(NodeID)}, Distance, {walk, encode(Walk)}, {direction, encode(Direction)}]} ||
         [{node, NodeID}, Distance, _Angle, {walk, Walk}, {direction, Direction}] <- Description],
-      NewFDesc = {[{description, FormattedDescription}]},
-      {ok, Json} = json:encode(NewFDesc),
-      Body = io_lib:format("~s", [binary_to_list(Json)]),
-      Req:ok({"text/plain", Body})
+      NewFDesc = {struct, [{description, FormattedDescription}]},
+      io:format("~p~n", [NewFDesc]),
+      Json = mochijson2:encode(NewFDesc),
+      io:format("~p~n", [Json]),
+      Req:ok({"text/plain;charset=utf-8", Json})
   catch
     _:X -> io:format("~p~n", [X])
   end;
+  
+respond("/json", _, Req) ->
+  Req:ok({"text/plain;charset=utf-8", mochijson2:encode(
+    {struct, [
+      {<<"bla">>, 2.3445432489034598053094539089085555},
+      {<<"blub">>, {struct,
+        [{asdf, encode("<adfsdlü>")}]
+      }}
+    ]})});
+    
+respond("/unicode", _, Req) ->
+  Req:ok({"text/plain;charset=utf-8", io_lib:format("~ts", ["adfsdlü"])});
 
 respond("/map", _Params, Req) ->
   Req:serve_file("ui.html", filename:absname("../www"));
@@ -53,7 +64,10 @@ respond(Path, _Params, Req) ->
 
 node_to_coords(Node) ->
   {Lat, Lon} = geodata:nodeid_to_coords(Node),
-  [{node, Node}, {lat, Lat}, {lon, Lon}].
+  {struct, [{node, Node}, {lat, Lat}, {lon, Lon}]}.
 
 nodes_to_coords(Path) ->
-  [{[{node, Node}, {lat, Lat}, {lon, Lon}]} || {Node, {Lat, Lon}} <- lists:zip(Path, geodata:nodes_to_coords(Path))].
+  [{struct, [{node, Node}, {lat, Lat}, {lon, Lon}]} || {Node, {Lat, Lon}} <- lists:zip(Path, geodata:nodes_to_coords(Path))].
+  
+encode(String) ->
+  unicode:characters_to_binary(io_lib:format("~ts", [String])).
