b7b911dcb369f1592f8f0aa17a89f649ac40fd50
Haykuhi <haykuhi@haykuhi.(none)>
2011-09-28 17:59:59 +0200

    see previous message

--- src/name_server.erl
+++ src/name_server.erl
@@ -3,6 +3,7 @@
 
 
 show_all_nodes() ->
+  ets:new(osm_names_to_nodes,[named_table, set, public]),    %%Tabelle wird angelegt
   First = ets:first(osm_nodes),
   show_all_nodes(First).
 
@@ -24,8 +25,16 @@ log_node(NodeID) ->
   {node,ID,Lat,Lon,Attributes} = Node,
   case show_name(Attributes) of
     [] -> ignore;
-    [Any] -> log(Any)
+    [Any] -> store_name(Any,ID)           %%log ist rausgemacht, anstattdessen store_name hinzugefügt (aus store)
   end.
-
+  
 show_name(Attributes) ->
-  [Value||{Key,Value}<-Attributes, Key == "name"].
+  [Value||{Key,Value}<-Attributes, Key == "name"].    
+
+store_name(Name,ID) ->                                %% aus store module
+  ets:insert(osm_names_to_nodes, {Name,ID}).
+
+lookup_name(Name) ->
+ Result = ets:lookup(osm_names_to_nodes, Name),
+ Result.
+
