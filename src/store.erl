bf5ab3828e836b684f3ab4a409711186ea728841
Mirko Kiefer <mail@mirkokiefer.com>
2011-10-03 22:46:01 +0200

    use separate hash for nodes, ways and node2ways

--- src/store.erl
+++ src/store.erl
@@ -25,29 +25,29 @@ serialize() ->
   serialized. 
 
 node2wayids(NodeID) ->
-  case eredis:q(routing, ["HGET", routing, node2way_key(NodeID)]) of
+  case eredis:q(routing, ["HGET", node2ways, NodeID]) of
     {ok,undefined} -> [];
     {ok, Ways} -> binary_to_term(Ways)
   end.
   
 lookup_way(WayID) ->
-  case eredis:q(routing, ["HGET", routing, way_key(WayID)]) of
+  case eredis:q(routing, ["HGET", ways, WayID]) of
     {ok,undefined} -> undefined;
     {ok, Way} -> binary_to_term(Way)
   end.
   
 lookup_node(NodeID) ->
-  case eredis:q(routing, ["HGET", routing, node_key(NodeID)]) of
+  case eredis:q(routing, ["HGET", nodes, NodeID]) of
     {ok,undefined} -> undefined;
     {ok, Node} -> binary_to_term(Node)
   end.
   
 store_way(Way=#way{id=ID, refs=Refs}) ->
   store_nodeids2wayid(Refs, ID),
-  eredis:q(routing, ["HSET", routing, way_key(ID), term_to_binary(Way)]).
+  eredis:q(routing, ["HSET", ways, ID, term_to_binary(Way)]).
   
 store_node(Node=#node{id=ID}) ->
-  eredis:q(routing, ["HSET", routing, node_key(ID), term_to_binary(Node)]).
+  eredis:q(routing, ["HSET", nodes, ID, term_to_binary(Node)]).
 
 store_nodeids2wayid([], _WayID) ->
   success;
@@ -57,13 +57,4 @@ store_nodeids2wayid([FirstNodeID|Rest], WayID) ->
   store_nodeids2wayid(Rest, WayID).
   
 store_nodeid2wayid(NodeID, WayID) ->
-  eredis:q(routing, ["HSET", routing, node2way_key(NodeID), term_to_binary([WayID|node2wayids(NodeID)])]).
-  
-node_key(ID) ->
-  "node_" ++ ID.
-  
-way_key(ID) ->
-  "way_" ++ ID.
-  
-node2way_key(ID) ->
-  "node2way" ++ ID.
+  eredis:q(routing, ["HSET", node2ways, NodeID, term_to_binary([WayID|node2wayids(NodeID)])]).
