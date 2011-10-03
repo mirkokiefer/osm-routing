f9ce01f285240e207e88b6c1b60368bc0ef8b9c4
Mirko Kiefer <mail@mirkokiefer.com>
2011-10-03 18:27:44 +0200

    fix issue with list of strings getting flattened to deep

--- src/utils.erl
+++ src/utils.erl
@@ -1,6 +1,6 @@
 -module(utils).
 
--export([float_to_string/1, deg2rad/1, rad2deg/1, intersection/2]).
+-export([float_to_string/1, deg2rad/1, rad2deg/1, intersection/2, flatten_once/1]).
 
 float_to_string(Float) ->
   [String] = io_lib:format("~p",[trunc(Float)]),
@@ -11,3 +11,12 @@ deg2rad(Deg) -> Deg*math:pi()/180.
 rad2deg(Rad) -> Rad*180/math:pi().
   
 intersection(L1,L2) -> lists:filter(fun(X) -> lists:member(X,L1) end, L2).
+
+flatten_once(ListOfLists) ->
+  flatten_once(ListOfLists, []).
+
+flatten_once([], NewList) ->
+  NewList;
+
+flatten_once([FirstList|Rest], NewList) ->
+  flatten_once(Rest, NewList++FirstList).
