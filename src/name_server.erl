f50c283d45c47b45f936223efbdb48cfd6011944
Haykuhi <haykuhi@haykuhi.(none)>
2011-10-05 08:22:32 +0200

    the recursive function extract_nodes starts from the first object in the list

--- src/name_server.erl
+++ src/name_server.erl
@@ -1,12 +1,12 @@
 -module(name_server).
--export([start/0,stop/0,lookup_name/1]).
+-export([start/0,stop/0,lookup_name/1,extract_nodes/1]).
 
 
 start() ->
-  ets:new(osm_names_to_nodes,[named_table, set, public]),    %%Tabelle wird angelegt; aus ets mudule new funktion nehmen
-  First = ets:first(osm_nodes),   %%erste node aus osm wird genommen; über first funktion, die im ets module definiert ist
+  ets:new(osm_names_to_nodes,[named_table, set, public]),    %%Tabelle wird angelegt; [stehen objects drin]; set heißt ein Schlüssel zu jedem Objekt; 
+  First = ets:first(osm_nodes),   %%public:Any process may read or write to the table.
   extract_nodes(First),
-  FirstWay = ets:first(osm_ways),   
+  FirstWay = ets:first(osm_ways),  
   extract_ways(FirstWay),
   success.   
       
@@ -14,31 +14,30 @@ stop() ->
   ets:delete(osm_names_to_nodes).
   
 
-extract_nodes(Previous) ->       %% rekursive Funktion, die alle nodes durchläuft
-  Next = ets:next(osm_nodes, Previous),  %%(next und first sind Funktionen, die man im ets benutzen kann; bis jetzt kann man nur duchlaufen)
+extract_nodes(CurrentNode) ->       %%Rekursive Funktion: rufe extract_nodes mit aktuellen Node auf
+  case CurrentNode of                                 
+    '$end_of_table' -> stop;     %%falls Tabelle zu Ende  -> stop
+    Any ->                         
+      check_node(CurrentNode),   
+      NextNode = ets:next(osm_nodes,CurrentNode),              
+      extract_nodes(NextNode)
+  end.      
   
-  case Next of                     %% pogramm stürzt ab, weil nicht weiß, was zu tun, wenn keine nodes mehr da; deshalb case                
-    '$end_of_table' -> stopp;      %% wenn next funktion kein next mehr hat, gibt sie das zitat aus, was bedeutet, dass stopp
-    Any ->                         %% wenn nicht, dann durchlaufe alle nodes
-      check_node(Next),              %% unten definiert; soll erst nur name und ids extrahieren und dann alle nodes mit genannten eigschaften anzeigen
-      extract_nodes(Next)         %% soll alle nodes zeigen() (ist die rekursive funktion aus oben), Funktionsaufruf
-  end.
-  
-log(Element) ->                          
+log(Element) ->                          %%Funktion, um auf Konsole node auszugeben
   io:format("Log: ~p~n", [Element]).
 
-check_node(NodeID) ->                           %%Funktion, um auf Konsole node auszugeben
-  Node = store:lookup_node(NodeID),           %%aus store module die lookup_node funktion
+check_node(NodeID) ->                         %%Funktion, die 
+  Node = store:lookup_node(NodeID),           %%aus store module die lookup_node funktion; lookup funktion gibt mir dir nodes aus
   {node,ID,Lat,Lon,Attributes} = Node,        %% node + Info erscheinen im Tupel in der Konsole 
   case extract_name(Attributes) of
-    [] -> ignore;
-    [Any] -> store_name(Any,[ID])           %%log ist rausgemacht, anstattdessen store_name hinzugefügt (aus store)(Any für irgendein name +id)
-  end.
+    [] -> ignore;                             %%falls Attribut leer oder keinen Namen
+    [Any] -> store_name(Any,[ID])              %%sonst rufe store_name auf
+  end.                                     
   
-extract_name(Attributes) ->                           
+extract_name(Attributes) ->                           %%list comprehension
   [Value||{Key,Value}<-Attributes, Key == "name"].    
 
-store_name(Name,ID) ->                                %% aus store module; schreiben dadurch in die Datenbamk rein; Info´s aus Konsole in Tabelle
+store_name(Name,ID) ->                                %%  schreiben in die Datenbank rein; speichern dadurch
   ets:insert(osm_names_to_nodes, {string:to_lower(Name),ID}).%%name immer klein
 
 lookup_name(Name) ->
@@ -46,17 +45,17 @@ lookup_name(Name) ->
                                              %%namens; Fkt. Aufruf; bevor er namen sucht, klein machen
 
 
-extract_ways(Previous) ->      
-  Next = ets:next(osm_ways, Previous),
- case Next of                                    
+extract_ways(CurrentNode) ->      
+ case CurrentNode of                                    
     '$end_of_table' -> stop;      
     Any ->                        
-      check_ways(Next),              
-      extract_ways(Next)        
+      check_ways(CurrentNode), 
+      NextNode = ets:next(osm_ways, CurrentNode),             
+      extract_ways(NextNode)        
   end.
 
 check_ways(WayID) ->                       
-  Way = store:lookup_way(WayID),           
+  Way = store:lookup_way(WayID),        %% in neue Tabelle die ways reinschreiben   
   {way,ID,Attributes,Nodes} = Way,  
   case extract_name(Attributes) of
     [] -> ignore;
